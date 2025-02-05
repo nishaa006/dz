b2ceed7f3a15fab0c444b46f13fe652ee57f05a7
Мария Григорьева <gma0609@mail.ru>
2025-02-05 18:13:08 +0300

    add new module and file README.md,

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,18 @@
-def filter_by_state(data, state='EXECUTED'):
+from datetime import datetime
+from typing import List, Dict, Any
+
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
     фильтрует список словарей по значению ключа 'state'.
     """
-    return [item for item in data if item.get('state') == state]
+    return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(data, descending=True):
+def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
     """
-    сортирует список словарей по ключу 'date'.
+    Сортирует список словарей по ключу 'date'.
     """
-    return sorted(data, key=lambda x: x.get('date'), reverse=descending)
+    return sorted(
+        data, key=lambda x: datetime.fromisoformat(x["date"]) if x.get("date") else datetime.min, reverse=descending
+    )
