3002b2060728fd96d8a2e96df2eb22972fead676
Grigoreva.M <maha234you@gmail.com>
2025-03-13 12:32:04 +0300

    добавила логирование в модули masks и utils

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,44 @@
+import logging
+import os
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('logs/masks.log', mode='w')
+file_handler.setLevel(logging.DEBUG)
+
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
 def get_mask_card_number(card_number: str) -> str:
+    logger.info(f"Начало маскировки номера карты: {card_number}")
     card_number = str(card_number)
 
-    # Формируем замаскированные блоки
     block_1 = card_number[:4]
     block_2 = card_number[4:6] + "**"
     block_3 = "****"
     block_4 = card_number[-4:]
 
-    # Объединяем блоки с пробелами
     masked_card = f"{block_1} {block_2} {block_3} {block_4}"
 
+    logger.info(f"Маскированный номер карты: {masked_card}")
     return masked_card
 
-
 def get_mask_account(number_acc: str) -> str:
+    logger.info(f"Начало маскировки номера счета: {number_acc}")
     if len(number_acc) < 4:
+        logger.warning(f"Некорректный номер счета: {number_acc}")
         return "Неверный номер счёта"
-    return f"**{number_acc[-4:]}"
+    masked_account = f"**{number_acc[-4:]}"
+    logger.info(f"Маскированный номер счета: {masked_account}")
+    return masked_account
+
+
+get_mask_account('28831920132347')
+get_mask_card_number('4214421122223333')
+
